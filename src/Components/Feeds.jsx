d2bfc450de7ee5bd67214f4b7e2cfa874907c747
SamsonGulma <sams0ngultek@gmail.com>
2025-02-23 03:14:55 +0300

    adjusting some ui features

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -8,215 +8,67 @@ const Feeds = () => {
   return (
       <div className='feeds-and-blogggs-container w-screen mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2 grid-flow-col'>
           
-        <div className='all-blogggs-for-you row-start-1 col-start-1 row-span-2 col-span-2'>
+        <div className='all-blogggs-for-you grid grid-cols-3 grid-rows-2 row-start-1 col-start-1 row-span-2 col-span-2'>
               
-          <div className='your-favorite-related-topics flex h-40 w-[25vw] border-1'>
-              
-                  <div className=' w-[70em] h-40 place-content-center bg-amber-700'>
+          <div className='your-favorite-related-topics row-start-1 col-start-1 row-span-2  border-1'>
+            
+            <div className='card-one  flex flex-col  border-1'>
+                      
+                <div className=' w-[17rem] h-40 place-content-center bg-amber-700'>
                     <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                 </div>
+                </div>
         
-              <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                  <h1 className='font-bold overflow-hidden pl-7'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  </h1>
+                <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
+                    <h1 className='font-bold overflow-hidden pl-7'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    </h1>
 
-                  <p className='overflow-hidden truncate pl-7'>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Libero esse aut asperiores enim, nam similique debitis nemo ad
-                      odit accusantium quis recusandae tempora suscipit obcaecati
-                      aspernatur. Rerum facere accusantium autem.
-                  </p>
+                    <p className='overflow-hidden truncate pl-7'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Libero esse aut asperiores enim, nam similique debitis nemo ad
+                        odit accusantium quis recusandae tempora suscipit obcaecati
+                        aspernatur. Rerum facere accusantium autem.
+                    </p>
 
-                  <div className='flex pl-7 pt-3'>
-                      <div className=''>
-                        <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
-                      </div>
+                    <div className='flex pl-7 pt-3'>
+                        <div className=''>
+                            <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
+                        </div>
 
-                      <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
-                  </div>
+                        <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
+                    </div>
 
-                  <div className='grid grid-cols-2 pl-7 pt-4'>
-                      <p className='font-extralight'>0sec ago</p>
-                      <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
-                  </div>
-                  </div>
+                    <div className='grid grid-cols-2 pl-7 pt-4'>
+                        <p className='font-extralight'>0sec ago</p>
+                        <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
+                    </div>
+                          
+                </div>
+            </div>
                      
-              </div>
+            </div>
 
 
-              <div className='your-favorite-related-topics flex h-40 w-[25vw] border-1'>
-              
-                  <div className=' w-[70em] h-40 place-content-center bg-amber-700'>
-                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                 </div>
-        
-              <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                  <h1 className='font-bold overflow-hidden pl-7'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  </h1>
-
-                  <p className='overflow-hidden truncate pl-7'>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Libero esse aut asperiores enim, nam similique debitis nemo ad
-                      odit accusantium quis recusandae tempora suscipit obcaecati
-                      aspernatur. Rerum facere accusantium autem.
-                  </p>
-
-                  <div className='flex pl-7 pt-3'>
-                      <div className=''>
-                        <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
-                      </div>
-
-                      <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
-                  </div>
-
-                  <div className='grid grid-cols-2 pl-7 pt-4'>
-                      <p className='font-extralight'>0sec ago</p>
-                      <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
-                  </div>
-                  </div>
-                     
-              </div>
-
-
-
-
-              <div className='your-favorite-related-topics flex h-40 w-[25vw] border-1'>
-              
-                  <div className=' w-[70em] h-40 place-content-center bg-amber-700'>
-                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                 </div>
-        
-              <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                  <h1 className='font-bold overflow-hidden pl-7'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  </h1>
-
-                  <p className='overflow-hidden truncate pl-7'>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Libero esse aut asperiores enim, nam similique debitis nemo ad
-                      odit accusantium quis recusandae tempora suscipit obcaecati
-                      aspernatur. Rerum facere accusantium autem.
-                  </p>
-
-                  <div className='flex pl-7 pt-3'>
-                      <div className=''>
-                        <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
-                      </div>
-
-                      <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
-                  </div>
-
-                  <div className='grid grid-cols-2 pl-7 pt-4'>
-                      <p className='font-extralight'>0sec ago</p>
-                      <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
-                  </div>
-                  </div>
-                     
-              </div>
-
-
-
-
-              <div className='your-favorite-related-topics flex h-40 w-[25vw] border-1'>
-              
-                  <div className=' w-[70em] h-40 place-content-center bg-amber-700'>
-                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                 </div>
-        
-              <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                  <h1 className='font-bold overflow-hidden pl-7'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  </h1>
-
-                  <p className='overflow-hidden truncate pl-7'>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Libero esse aut asperiores enim, nam similique debitis nemo ad
-                      odit accusantium quis recusandae tempora suscipit obcaecati
-                      aspernatur. Rerum facere accusantium autem.
-                  </p>
-
-                  <div className='flex pl-7 pt-3'>
-                      <div className=''>
-                        <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
-                      </div>
-
-                      <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
-                  </div>
-
-                  <div className='grid grid-cols-2 pl-7 pt-4'>
-                      <p className='font-extralight'>0sec ago</p>
-                      <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
-                  </div>
-                  </div>
-                     
-              </div>
-
-
-
-
-              <div className='your-favorite-related-topics flex h-40 w-[25vw] border-1'>
-              
-                  <div className=' w-[70em] h-40 place-content-center bg-amber-700'>
-                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                 </div>
-        
-              <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                  <h1 className='font-bold overflow-hidden pl-7'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  </h1>
-
-                  <p className='overflow-hidden truncate pl-7'>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Libero esse aut asperiores enim, nam similique debitis nemo ad
-                      odit accusantium quis recusandae tempora suscipit obcaecati
-                      aspernatur. Rerum facere accusantium autem.
-                  </p>
-
-                  <div className='flex pl-7 pt-3'>
-                      <div className=''>
-                        <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
-                      </div>
-
-                      <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
-                  </div>
-
-                  <div className='grid grid-cols-2 pl-7 pt-4'>
-                      <p className='font-extralight'>0sec ago</p>
-                      <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
-                  </div>
-                  </div>
-                     
-              </div>
-
-
-
-
-
-
-              
-
-              <div>
+            <div>
                   
               <hr className='mt-4 md-4 ml-10 mr-10'/>
-              </div>
+            </div>
 
 
-                <div className='all-the-rest-blogggs row-span-2 col-span-2'>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam ab fugit tempora magnam! Modi a autem minima, nostrum eaque assumenda provident praesentium, porro vero, ex delectus natus facere rerum sequi!
-                    </p>
-                </div>
+            <div className='all-the-rest-blogggs row-span-2 col-span-2'>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam ab fugit tempora magnam! Modi a autem minima, nostrum eaque assumenda provident praesentium, porro vero, ex delectus natus facere rerum sequi!
+                </p>
+            </div>
           </div>
 
 
           <div>
-              <PeopleTrendYourtopic />
+            <PeopleTrendYourtopic />
               
           </div>
 
-      </div>
+    </div>
       
   )
 }
