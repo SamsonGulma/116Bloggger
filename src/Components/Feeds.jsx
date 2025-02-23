4a3074d7e721a8488ca809d5109af4e2a523a739
SamsonGulma <sams0ngultek@gmail.com>
2025-02-23 04:10:37 +0300

    adjusting some ui features

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -6,41 +6,41 @@ import PeopleTrendYourtopic from './PeopleTrendYourtopic'
 
 const Feeds = () => {
   return (
-      <div className='feeds-and-blogggs-container w-screen mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2 grid-flow-col'>
+      <div className='feeds_and_blogggs_container w-screen mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2 grid-flow-col'>
           
-        <div className='all-blogggs-for-you grid grid-cols-3 grid-rows-2 row-start-1 col-start-1 row-span-2 col-span-2'>
+        <div className='all_blogggs_for_you grid grid-cols-3 grid-rows-[auto_1fr] row-start-1 col-start-1 row-span-2 col-span-2'>
               
-          <div className='your-favorite-related-topics row-start-1 col-start-1 row-span-2  border-1'>
+          <div className='your_favorite_related-topics row-start-1 col-start-1 row-span-1 relative'>
             
-            <div className='card-one  flex flex-col  border-1'>
+                  <div className='card_one flex flex-col  border-1'>
                       
-                <div className=' w-[17rem] h-40 place-content-center bg-amber-700'>
-                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
-                </div>
-        
-                <div className='title-content-authorImg-author-time-bookmark  overflow-hidden'>
-                    <h1 className='font-bold overflow-hidden pl-7'>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    </h1>
-
-                    <p className='overflow-hidden truncate pl-7'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Libero esse aut asperiores enim, nam similique debitis nemo ad
-                        odit accusantium quis recusandae tempora suscipit obcaecati
-                        aspernatur. Rerum facere accusantium autem.
-                    </p>
-
-                    <div className='flex pl-7 pt-3'>
+                      <div className='flex pl-2 pt-3'>
                         <div className=''>
                             <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
                         </div>
 
                         <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste</h4>
                     </div>
+                      
+                <div className='place-content-center bg-amber-700 ml-2'>
+                    <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
+                </div>
+        
+                <div className='title_content_authorImg_author_time_bookmark  overflow-hidden'>
+                    <h1 className='font-bold overflow-hidden pl-2'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    </h1>
 
-                    <div className='grid grid-cols-2 pl-7 pt-4'>
+                    <p className='overflow-hidden truncate pl-2'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Libero esse aut asperiores enim, nam similique debitis nemo ad
+                        odit accusantium quis recusandae tempora suscipit obcaecati
+                        aspernatur. Rerum facere accusantium autem.
+                    </p>
+
+                    <div className='grid grid-cols-2 auto-rows-auto ml-2 mt-4 mb-4 '>
                         <p className='font-extralight'>0sec ago</p>
-                        <MdBookmark className=' h-5 w-5 self-center text-right float-right justify-self-end mr-7'/>
+                        <MdBookmark className=' h-5 w-5 self-center text-right justify-self-end mr-7'/>
                     </div>
                           
                 </div>
@@ -48,14 +48,7 @@ const Feeds = () => {
                      
             </div>
 
-
-            <div>
-                  
-              <hr className='mt-4 md-4 ml-10 mr-10'/>
-            </div>
-
-
-            <div className='all-the-rest-blogggs row-span-2 col-span-2'>
+            <div className='all_the_rest_blogggs row-start-2 row-span-2 col-start-1 col-span-2'>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam ab fugit tempora magnam! Modi a autem minima, nostrum eaque assumenda provident praesentium, porro vero, ex delectus natus facere rerum sequi!
                 </p>
