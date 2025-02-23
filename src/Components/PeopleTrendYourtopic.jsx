777b08b9724cce08209552df417331bcb6bd4387
SamsonGulma <sams0ngultek@gmail.com>
2025-02-24 02:00:25 +0300

    adding some ui features and Adding some Components

--- src/Components/PeopleTrendYourtopic.jsx
+++ src/Components/PeopleTrendYourtopic.jsx
@@ -4,27 +4,45 @@ import {FaLeaf} from 'react-icons/fa'
 
 const PeopleTrendYourtopic = () => {
   return (
-      <div className='border-l-4 border-gray-100 h-full self-center'>
-        <div>
-          <MdPeople />
-        <h1>People who to follow</h1>
+      <div className='border-l-4 border-gray-100 h-fullr'>
+        <div className='people-to-follow '>
           <div>
-            <img src="" alt="" />
-          <h3>Samson Gulma Tekeste</h3>
-          <button type="">Follow</button>
+            <MdPeople />
+            <h1>People who to follow</h1>
           </div>
+          <div className='flex flex-row'>
+            <img src="" alt="" />
+            <h3>Samson Gulma Tekeste</h3>
+            <button>Follow</button>
+          </div>
+          <a href="">See the full list</a>
         </div>
 
           <hr />
+        <div className='trendingup'>
           <div>
-        <MdTrendingUp />
-        <h1>Today&apos;s top trends</h1>
+            <MdTrendingUp />
+            <h1>Today&apos;s top trends</h1>
           </div>
+          <div>
+            <p>By</p>
+            <img src="" alt="" />
+            <h3>Samson Gulma</h3>
+          </div>
+          <a href="">See the full list</a>
+
+        </div>
 
           <hr />
+        <div className='topics'>
           <div>
-              <FaLeaf/>
+            <FaLeaf />
+            <h1>Topics for you from Our Team.</h1>  
+            <button>Programming</button>
           </div>
+          <a href="">See the full list</a>
+
+        </div>
           
     </div>
   )
