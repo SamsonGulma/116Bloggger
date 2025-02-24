06d12f609fd72170afbd1e072d165814836de2ff
SamsonGulma <sams0ngultek@gmail.com>
2025-02-24 05:07:23 +0300

    Some design Adjustments

--- src/Components/AllTheRestBlogggsHero.jsx
+++ src/Components/AllTheRestBlogggsHero.jsx
@@ -1,8 +1,45 @@
 import 'react'
+import { MdBookmark } from 'react-icons/md'
+
+
 
 const AllTheRestBlogggsHero = () => {
   return (
-    <div>AllTheRestBlogggsHero</div>
+      <div>
+          <div className='flex pl-2 pt-3'>
+                      <div className=''>
+                          <img className=' h-[30px] w-[30px] bg-black rounded-lg self-center' src="" alt="" />
+                      </div>
+          
+                       <h4 className='overflow-hidden truncate self-center pl-2'>Samson Gulma Tekeste 1</h4>
+                  </div>
+                              
+                  <div className='place-content-center bg-amber-700 ml-2 mt-2 mr-2'>
+                      <img className=' overflow-hidden h-full ' src="../../public/Images/screen.png" alt="" />
+                  </div>
+                    
+                  <div className='title_content_authorImg_author_time_bookmark  overflow-hidden'>
+                      <h1 className='font-extrabold overflow-hidden pl-2 '>
+                           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti beatae et repellat ea ad
+                      </h1>
+          
+                      <p className='overflow-hidden pl-2 pr-2'>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                          Possimus exercitationem quasi dolor, quis, rerum odio, a tenetur
+                          deleniti error voluptatibus voluptatem. Sapiente labore odio corporis,
+                          ecusandae totam eveniet cum fugit.
+                      </p>
+                  </div>
+                                
+                  <div className='flex flex-row'>
+                      <p className='underline decoration-dotted pl-2 font-semibold'>Read more -&gt;</p>
+                  </div>
+                          
+                  <div className='grid grid-cols-2 auto-rows-auto ml-2 mt-4 mb-4 '>
+                      <p className='font-extralight'>0sec ago</p>
+                      <MdBookmark className=' h-5 w-5 self-center text-right justify-self-end mr-7'/>
+                   </div>
+    </div>
   )
 }
 
--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -2,21 +2,19 @@ import 'react'
 import '../assets/Styles/Feeds.css'
 import PeopleTrendYourtopic from './PeopleTrendYourtopic'
 import Heros from './FavoriteTopicsHero'
-
+import AllTheRestBlogggsHero from './AllTheRestBlogggsHero'
 
 
 const Feeds = () => {
   return (
-      <div className='feeds_and_blogggs_container w-full mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2 grid-flow-col'>
+      <div className='feeds_and_blogggs_container mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3'>
           
-        <div className='all_blogggs_for_you grid grid-cols-3 grid-rows-[auto_1fr] row-start-1 col-start-1 row-span-2 col-span-2'>
+        <div className='all_blogggs_for_you grid grid-cols-3 row-start-1 col-start-1 row-span-2 col-span-2'>
               
-          <div className='your_favorite_topics row-start-1 col-start-1 row-span-1'> <Heros/> </div>
+          <div className='your_favorite_topics  col-start-1 row-span-1'> <Heros/> </div>
 
-            <div className='all_the_rest_blogggs row-start-2 row-span-2 col-start-1 col-span-2'>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam ab fugit tempora magnam! Modi a autem minima, nostrum eaque assumenda provident praesentium, porro vero, ex delectus natus facere rerum sequi!
-                </p>
+            <div className='all_the_rest_blogggs'>
+                <AllTheRestBlogggsHero/>
               </div>
               
           </div>
