0cb4e69b746be819802c22c39861ae4ab061b7e4
SamsonGulma <sams0ngultek@gmail.com>
2025-02-26 01:15:54 +0000

    Feb 26, 2025, 4:15 AM

--- src/Components/FavoriteTopicsHero.jsx
+++ src/Components/FavoriteTopicsHero.jsx
@@ -8,7 +8,7 @@ const Heros = () => {
         <div className='fav_card_one'>
             {
             blogggs.map((bloggg) => (
-                <div key={''} className='flex flex-col md: min-h-0 h-auto shadow-2xl rounded-2xl border-1 border-gray-200'>
+                <div key={''} className='flex flex-col sm:flex-row min-h-0 h-auto shadow-2xl rounded-2xl border-1 border-gray-200'>
                 
                     <div className='flex pl-7 pt-3'>
                         <div className=''>
