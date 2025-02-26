3bd799bc6b0dacc159be8b057e6eb2c70024d2ec
SamsonGulma <sams0ngultek@gmail.com>
2025-02-26 01:19:50 +0000

    Feb 26, 2025, 4:19 AM

--- src/Components/FeedsContainer.jsx
+++ src/Components/FeedsContainer.jsx
@@ -7,7 +7,7 @@ import AllTheRestBlogggsHero from './AllTheRestBlogggsHero'
 
 const Feeds = () => {
   return (
-    <div className='feeds_and_blogggs_container mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2'>
+    <div className='feeds_and_blogggs_container mt-[10vh] ml-[5vw] pt-[7vh] pl-[5vw] border border-black grid grid-cols-3 grid-rows-2 w-screen'>
         <div className='all_blogggs_for_you flex flex-col row-start-1 col-start-1 row-span-2 col-span-2'>
           <div className='your_favorite_topics  col-start-1 col-end-1 row-start-1 row-span-1'> <FavoriteTopicsHero /> </div>
           <hr />
