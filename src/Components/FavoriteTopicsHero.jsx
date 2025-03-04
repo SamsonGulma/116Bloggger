c7862c1aa61606be7452d7ce01fb8c8c927a038b
SamsonGulma <sams0ngultek@gmail.com>
2025-03-04 23:18:52 +0000

    Mar 5, 2025, 2:18 AM

--- src/Components/FavoriteTopicsHero.jsx
+++ src/Components/FavoriteTopicsHero.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 const FavoriteTopicsHero = () => {
    const favoriteTopics = blogggs.slice(0, 2);
-    const [showFullContentText, setShowFullContentText] = useState(false)
+    const [showFullContentText, setShowFullContentText] = useState(true)
 
     let contentText = blogggs.content_text;
 
